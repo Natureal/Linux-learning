411bdaf241747774b32786cdad42338dfd8f4385
Natureal <naturechenpeng@gmail.com>
2019-07-23 18:28:30 +0900

    Tue Jul 23 18:28:30 JST 2019

---
+++ tail.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+// 实现了 Linux shell 中 tail 的功能，支持 -n 参数
+
+int parse_opt(int argc, char *argv[]){
+	const char *optstr = "n:";
+	char opt;
+	while((opt = getopt(argc, argv, optstr)) != -1){
+		return atoi(optarg);
+	}
+	return -1;
+}
+
+int main(int argc, char *argv[]){
+	int n = parse_opt(argc, argv);
+	FILE *fin = fopen(argv[argc - 1], "r");
+	if(!fin) exit(1);
+	char c;
+	if(n == 0){
+		return 0;
+	}
+	if(n < 0){
+		while((c = fgetc(fin)) != EOF) putchar(c);
+		return 1;
+	}
+	fseek(fin, 0, SEEK_END);
+	int p = ftell(fin), cnt = 0;
+	while(p){
+		// 由于文件末尾有一个 EOF 符号，因此要先 --p
+		fseek(fin, --p, SEEK_SET);
+		c = fgetc(fin);
+		// 每一行最后都有 \n
+		if(c == '\n'){
+			if(cnt++ == n) break;
+		}
+	}
+	// 下面这句是为了处理 p == 0 时，fgetc 把文件位置指针往后挪了一位的问题
+	if(!p) fseek(fin, 0, SEEK_SET);
+	while((c = fgetc(fin)) != EOF) putchar(c);
+	return 0;
+}
